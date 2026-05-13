7a47abed5ea13e09710681908448b9beb8803575
Cavad <javadmsh-apa103@code.edu.az>
2026-05-14 00:46:20 +0400

    adminpanelardi

--- 27-FrontToBackSqlConnection/obj/Debug/net10.0/FrontToBackSqlConnection.AssemblyInfo.cs
+++ 27-FrontToBackSqlConnection/obj/Debug/net10.0/FrontToBackSqlConnection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontToBackSqlConnection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+952ad20d5b38900e508d9c17e90562cccd460843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0450255025755d11e9dd2d223b94ad308dafeee")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontToBackSqlConnection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontToBackSqlConnection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
