c1df8626f2671227dd68ee9a84c5cfb8611d4603
Cavad <javadmsh-apa103@code.edu.az>
2026-05-19 01:19:16 +0400

    product

---
+++ 27-FrontToBackSqlConnection/Areas/AdminPanel/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using FrontToBackSqlConnection.Areas.AdminPanel.ViewModels;
+using FrontToBackSqlConnection.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrontToBackSqlConnection.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public ProductController(AppDbContext context,IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products=await _context.Products
+                .Where(p=>!p.isDeleted)
+                .Include(p=>p.ProductImages)
+                .Include(p=>p.Category)
+                .Select(p=>new ProductGetVM
+                {
+                    Id=p.Id,
+                    Name=p.Name,
+                    Price=p.Price,
+                    SKU=p.SKU,
+                    Image=p.ProductImages.FirstOrDefault().Image,
+                    CategoryName=p.Category.Name
+                })
+                .ToListAsync();
+
+            return View(products);
+        }
+    }
+}
--- 27-FrontToBackSqlConnection/obj/Debug/net10.0/FrontToBackSqlConnection.AssemblyInfo.cs
+++ 27-FrontToBackSqlConnection/obj/Debug/net10.0/FrontToBackSqlConnection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontToBackSqlConnection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a47abed5ea13e09710681908448b9beb8803575")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a48031cd3c860bf1565cc2e0520549b23b759c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontToBackSqlConnection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontToBackSqlConnection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
